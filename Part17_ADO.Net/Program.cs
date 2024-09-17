ae85d4c3f3fbe3efedcc2e5bf63dd06058d3f846
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2024-09-17 15:02:50 +0700

    add new console project for adonet

---
+++ Part17_ADO.Net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part17_ADO.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
