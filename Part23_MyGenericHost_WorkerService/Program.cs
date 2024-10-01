0759b6c6269ad0f19a0c62414abd41e80c27cbd1
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2024-10-01 17:55:48 +0700

    add some code for c# generic host

--- Part23_MyGenericHost_WorkerService/Program.cs
+++ Part23_MyGenericHost_WorkerService/Program.cs
@@ -1,14 +1,16 @@
+
 namespace Part23_MyGenericHost_WorkerService
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = Host.CreateApplicationBuilder(args);
             builder.Services.AddHostedService<Worker>();
 
             var host = builder.Build();
-            host.Run();
+            //host.Run();
+            await host.RunAsync();
         }
     }
 }
