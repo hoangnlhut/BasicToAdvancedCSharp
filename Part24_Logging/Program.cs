b4c16c1447324c50f014abe987b043e0286e6788
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2024-10-03 14:59:26 +0700

    add new project for logging

---
+++ Part24_Logging/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Part24_Logging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+            ILogger logger = factory.CreateLogger("Program");
+            logger.LogInformation("Hello World! Logging is {Description}.", "fun");
+        }
+    }
+}
