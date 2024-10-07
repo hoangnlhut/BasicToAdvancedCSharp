7a025a70b5256cb0471b651734ceed926a6e17cb
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2024-10-07 08:58:18 +0700

    Initiate attributes console app

---
+++ Part25Attributes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part25Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
