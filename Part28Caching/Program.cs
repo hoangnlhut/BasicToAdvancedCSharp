b175f516988d9daeb6bd8a93b5c2672a9d68e525
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2024-10-10 15:37:57 +0700

    init for new project of caching

---
+++ Part28Caching/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Part28Caching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddMemoryCache();
+        }
+    }
+}
