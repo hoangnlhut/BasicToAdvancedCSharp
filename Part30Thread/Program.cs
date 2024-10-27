3e6717a9ae78380822cd1fb8db6331f5627931a6
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2024-10-27 18:02:54 +0700

    start new project Thread

---
+++ Part30Thread/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part30Thread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
