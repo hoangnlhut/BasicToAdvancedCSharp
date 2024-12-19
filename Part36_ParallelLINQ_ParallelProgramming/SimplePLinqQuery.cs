528946d93f6ead2a0221919eb62a91d76c5a6542
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2024-12-19 17:55:14 +0700

    test some code compare asParallel and run in sequence

--- Part36_ParallelLINQ_ParallelProgramming/SimplePLinqQuery.cs
+++ Part36_ParallelLINQ_ParallelProgramming/SimplePLinqQuery.cs
@@ -21,48 +21,49 @@ namespace Part36_ParallelLINQ_ParallelProgramming
             // Result sequence might be out of order.
             var parallelQuery =
                 from num in source.AsParallel()
-                where num % 10 == 0
+                where num % 2 == 0
                 select num;
             stopwatch.Stop();
             Console.WriteLine($"parallelQuery finish in {stopwatch.Elapsed}");
 
 
             stopwatch.Start();
-            // Result sequence might be out of order.
-            var asEnumerable =
-                from num in source.AsEnumerable()
-                where num % 10 == 0
-                select num;
+            List<int> list =new List<int>();
+            foreach( int num in source)
+            {
+                if (num % 2 == 0) list.Add(num);
+            }
             stopwatch.Stop();
             Console.WriteLine($"asEnumerable finish in {stopwatch.Elapsed}");
 
+            #region other ways
+            //// Process result sequence in parallel
+            //parallelQuery.ForAll((e) => DoSomething(e));
 
-            // Process result sequence in parallel
-            parallelQuery.ForAll((e) => DoSomething(e));
+            //// Or use foreach to merge results first.
+            //foreach (var n in parallelQuery)
+            //{
+            //    Console.WriteLine(n);
+            //}
 
-            // Or use foreach to merge results first.
-            foreach (var n in parallelQuery)
-            {
-                Console.WriteLine(n);
-            }
+            //// You can also use ToArray, ToList, etc as with LINQ to Objects.
+            //var parallelQuery2 =
+            //    (from num in source.AsParallel()
+            //     where num % 10 == 0
+            //     select num).ToArray();
 
-            // You can also use ToArray, ToList, etc as with LINQ to Objects.
-            var parallelQuery2 =
-                (from num in source.AsParallel()
-                 where num % 10 == 0
-                 select num).ToArray();
+            //foreach (var n in parallelQuery2)
+            //{
+            //    DoSomething(n);
+            //}
 
-            foreach (var n in parallelQuery2)
-            {
-                DoSomething(n);
-            }
-
-            // Method syntax is also supported
-            var parallelQuery3 =
-                source.AsParallel()
-                    .Where(n => n % 10 == 0)
-                    .Select(n => n);
-            Console.WriteLine(parallelQuery3);
+            //// Method syntax is also supported
+            //var parallelQuery3 =
+            //    source.AsParallel()
+            //        .Where(n => n % 10 == 0)
+            //        .Select(n => n);
+            //Console.WriteLine(parallelQuery3);
+            #endregion
 
             Console.WriteLine("\nPress any key to exit...");
             Console.ReadLine();
