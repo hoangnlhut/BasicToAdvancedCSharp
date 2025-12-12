9e147ecf8b44e68c8998f4eaa50481bd2e205490
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-12-12 15:01:00 +0700

    A sample for test child class override method of base class (implement an interface)

---
+++ Part11OOP/DeniInterface.cs
@@ -0,0 +1,89 @@
+﻿using Part11_12OOP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part11_12OOP
+{
+    interface IStorable
+    {
+        void Read();
+        void Write();
+    }
+
+    // lớp Document đơn giản thực thi giao diện IStorable
+    public class Document : IStorable
+    {
+        // bộ khởi dựng
+        public Document(string s)
+        {
+            Console.WriteLine("Creating document with: {0}", s);
+        }
+        // đánh dấu phương thức Read ảo
+        public virtual void Read()
+        {
+            Console.WriteLine("Document Read Method for IStorable");
+        }
+
+
+        // không phải phương thức ảo
+        public void Write()
+        {
+            Console.WriteLine("Document Write Method for IStorable");
+        }
+    }
+
+    // lớp dẫn xuất từ Document
+    public class Note : Document
+    {
+        public Note(string s) : base(s)
+        {
+            Console.WriteLine("Creating note with: {0}", s);
+        }
+        // phủ quyết phương thức Read()
+        public override void Read()
+        {
+            Console.WriteLine("Overriding the Read Method for Note!");
+        }
+        // thực thi một phương thức Write riêng của lớp
+        public void Write()
+        {
+            Console.WriteLine("Implementing the Write method for Note!");
+        }
+    }
+
+    public static class DeniInterface
+    {
+        public static void Run()
+        {
+            // tạo một đối tượng Document
+            Document theNote = new Note("Test Note");
+            IStorable isNote = theNote as IStorable;
+            if (isNote != null)
+            {
+                isNote.Read();
+                isNote.Write();
+            }
+            Console.WriteLine("\n");
+            // trực tiếp gọi phương thức
+            theNote.Read();
+            theNote.Write();
+            Console.WriteLine("\n");
+            // tạo đối tượng Note
+            Note note2 = new Note("Second Test");
+            IStorable isNote2 = note2 as IStorable;
+            if (isNote2 != null)
+            {
+                isNote2.Read();
+                isNote2.Write();
+            }
+            Console.WriteLine("\n");
+            // trực tiếp gọi phương thức
+            note2.Read();
+            note2.Write();
+        }
+    }
+}
--- Part11OOP/Program.cs
+++ Part11OOP/Program.cs
@@ -4,10 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test polymorphism");
-            Polymorphism polymorphism = new Polymorphism();
-            polymorphism.ImplementWrong();
-            polymorphism.ImplementTrue();
+            //Console.WriteLine("Test polymorphism");
+            //Polymorphism polymorphism = new Polymorphism();
+            //polymorphism.ImplementWrong();
+            //polymorphism.ImplementTrue();
+
+            //TestCarPolymophy();
+            DeniInterface.Run();
+        }
+
+
+        static void TestCarPolymophy()
+        {
+            Car[] cars = new Car[4];
+            cars[0] = new Car("Base car");
+            cars[1] = new SportsCar("BMW");
+            cars[2] = new VintageCar("Mercedes");
+            cars[3] = new HyperCar("Lamboghini");
+
+            foreach (Car car in cars)
+            {
+                car.Who();
+            }
         }
     }
+
 }
+
